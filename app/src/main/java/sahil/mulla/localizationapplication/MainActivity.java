1a72d119df200c8f8ebc5104365a34c69135d9a2
sahil mulla <sahilmulla199@gmail.com>
2020-09-07 16:20:18 +0530

    added menu

--- app/src/main/java/sahil/mulla/localizationapplication/MainActivity.java
+++ app/src/main/java/sahil/mulla/localizationapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package sahil.mulla.localizationapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +16,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+      switch (id)
+      {
+          case R.id.englishId:
+              Toast.makeText(this, "English", Toast.LENGTH_SHORT).show();
+              break;
+          case R.id.hindiId:
+              Toast.makeText(this, "Hindi", Toast.LENGTH_SHORT).show();
+              break;
+
+      }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        inflater.inflate(R.menu.menufile, menu);
         return true;
     }
 }
